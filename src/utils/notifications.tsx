306917dd33e075caac1b37c782a79a0d4bcc6bc4
Matheus de Assis <assis4002@gmail.com>
2023-01-05 21:52:02 -0300

    notifications update

--- src/utils/notifications.tsx
+++ src/utils/notifications.tsx
@@ -1,15 +1,20 @@
 import { Platform } from "react-native";
 import { showMessage } from "react-native-flash-message";
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from "react-native-responsive-fontsize";
 
 export function notifySucccess(message: string){
     showMessage({
         message: message,
         type: "success",
         icon: "success",
+        backgroundColor: "#19efb7",
         duration: 500,
         style: {
-            paddingTop: Platform.OS === 'ios' ? 10 : 50
+            borderRadius: RFValue(20),
+            marginTop: Platform.OS === 'ios' ? RFValue(20) : RFValue(30),
+            marginLeft: RFValue(15),
+            marginRight: RFValue(15)
         }
     });
 }
